0805608681497dd40236c60b64a04bbf8cc93d1d
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-04-25 02:22:56 +0800

    Set up semantic HTML

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>Menu</li>
+                <li>Book</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
