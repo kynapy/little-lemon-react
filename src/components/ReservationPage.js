553a939f74e29b00be521549426e85c564ad9da1
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-04-30 03:20:55 +0800

    Add code support for react-router-dom

--- src/components/ReservationPage.js
+++ src/components/ReservationPage.js
@@ -1,7 +1,13 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import Hero from "./Hero";
+
 const ReservationPage = props => {
     return (
         <div>
-            <p>Reservations</p>
+            <Header />
+            <Nav />
+            <p>Reservations page</p>
         </div>
     )
 }
