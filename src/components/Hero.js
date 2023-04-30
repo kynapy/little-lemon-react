c0479aceaa8ff301129858d881e15bedcae3b6d9
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-04-30 23:53:30 +0800

    Complete Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,46 @@
+import heroImage from "./../images/heroImage.jpg";
+
 const Hero = props => {
     return (
         <div
+            id="hero"
             style={{
                 backgroundColor: "#495E57",
                 textAlign: "center",
                 display: "grid",
-                padding: "1rem",
-                gridTemplateColumns: "2fr 1fr",
-            }}>
-            <div>
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <p>{ props.paragraph }</p>
-                <button style={{borderRadius: 8}}>Book A Table Now</button>
+                padding: "1vh 15vw 2vh 15vw",
+                gridTemplateColumns: "4fr 3fr",
+                columnGap: "1vw",
+                }}
+            >
+            <div style={{
+                    textAlign: "left",
+                }}
+            >
+                <div style={{lineHeight: 0.3}}>
+                    <h2 style={{ color:"#F4CE14" }}>Little Lemon</h2>
+                    <h4 style={{ color:"#FFFFFF" }}>Chicago</h4>
+                </div>
+                <p style={{ color:"#FFFFFF", fontSize: 16, lineHeight: 1.4 }}>{ props.paragraph }</p>
+                <button
+                    style={{
+                        borderRadius: 6,
+                        padding: "10px 18px",
+                        backgroundColor: "#F4CE14",
+                        border: "0px solid #000000",
+                    }}
+                >
+                    Book A Table Now
+                </button>
             </div>
-            <div>
-                <p>Image</p>
+            <div style={{placeItems: "center", display:"grid"}}>
+                <img
+                    src={ heroImage }
+                    style={{
+                        maxHeight: "18vh",
+                        borderRadius: 6,
+                    }}
+                ></img>
             </div>
         </div>
     )
