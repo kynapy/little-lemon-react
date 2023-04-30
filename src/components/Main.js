2c93320e796476c4a8236b15efdfe87b632a73f4
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-04-30 23:53:03 +0800

    Add styling for Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,16 @@
+import "./css/Main.css";
 import Hero from "./Hero.js";
-import Testimonials from "./Testimonials.js";
-import Highlights from "./Highlights.js";
+import TestimonialSection from "./TestimonialSection.js";
+import HighlightSection from "./HighlightSection.js";
 import About from "./About.js";
 
 function Main() {
     const heroParagraph = "Founded in 1987, the Little Lemon restaurant has been serving the people of Portsmouth Street traditional Italian dishes, with recipes crafted and passed down upon 3 generations.";
     return (
         <main>
-            <Hero paragraph={ heroParagraph }/>
-            <Highlights />
-            <Testimonials />
+            <Hero id="hero" paragraph={ heroParagraph }/>
+            <HighlightSection />
+            <TestimonialSection />
             <About />
         </main>
     )
