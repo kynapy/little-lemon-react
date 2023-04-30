d056a7fc6d2f293d49c9007e31db6ab106fd3281
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-05-01 04:36:54 +0800

    Add styling for About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,17 @@
 const About = props => {
     return (
         <div
+            id="about-section"
             style={{
                 display: "grid",
                 gridTemplateColumns: "1fr 1fr",
-                padding: "1vh 10vw",
+                padding: "4vh 15vw",
                 placeItems: 'center',
             }}
         >
-            <div>
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
+            <div style={{lineHeight: 1}}>
+                <p style={{fontSize: 26, lineHeight: 0}}><b>Little Lemon</b></p>
+                <p style={{fontSize: 20, lineHeight: 0.3}}>Chicago</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
             <div>
