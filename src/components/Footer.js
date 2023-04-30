d03b1f7804cc9d1e3aa46443837960de4947f36b
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-04-30 23:53:18 +0800

    Add footer skeleton

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,41 @@
 function Footer() {
     return (
-        <footer>footer</footer>
+        <footer style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 4fr",
+            padding: "2vh 10vw",
+            columnGap: "3vw",
+            backgroundColor: "#EDEFEE",
+        }}>
+            <div>Image</div>
+            <div style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr 1fr",
+                columnGap: "1vw",
+            }}>
+                <div>
+                    <p><b>Navigation</b></p>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Menu</p>
+                    <p>Reservation</p>
+                    <p>Order Online</p>
+                    <p>Login</p>
+                </div>
+                <div>
+                    <p><b>Contact</b></p>
+                    <p>Address</p>
+                    <p>Phone Number</p>
+                    <p>Email</p>
+                </div>
+                <div>
+                    <p><b>Social Media</b></p>
+                    <p>Instagram</p>
+                    <p>Twitter</p>
+                    <p>Facebook</p>
+                </div>
+            </div>
+        </footer>
     )
 }
 
