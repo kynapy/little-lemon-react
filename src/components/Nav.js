5d7063461e093f2c1883d3df83b827430b65cfce
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-04-30 23:53:43 +0800

    Add code for routing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,26 @@
 import "./css/Nav.css";
 import Logo from "./../images/Logo.svg";
+import { Route, Routes, Link } from "react-router-dom";
+import App from "../App";
+import ReservationPage from "./ReservationPage";
 
 function Nav() {
     return (
-        <nav>
-            <ul>
-                <li><img src={Logo}></img></li>
-                <li>About</li>
-                <li>Menu</li>
-                <li>Reservations</li>
-                <li>Order Online</li>
-                <li>Login</li>
-            </ul>
-        </nav>
+        <div>
+            <nav>
+                <ul>
+                    <li><img src={ Logo } alt="Little Lemon Logo"></img></li>
+                    <li><Link>About</Link></li>
+                    <li><Link>Menu</Link></li>
+                    <li><Link to="/reservation-page">Reservations</Link></li>
+                    <li><Link>Order Online</Link></li>
+                    <li><Link>Login</Link></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/reservation-page" element={ <ReservationPage /> } />
+            </Routes>
+        </div>
     )
 }
 
