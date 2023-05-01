a84de0bf755fdc1cacc748e285365674edd596f0
Kiyan Ang Ping Young <apyoung11@hotmail.com>
2023-05-01 19:55:35 +0800

    Add links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,41 +3,52 @@ import "./css/Footer.css";
 
 function Footer() {
     return (
-        <footer style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 4fr",
-            padding: "2vh 10vw",
-            columnGap: "3vw",
-            backgroundColor: "#EDEFEE",
-        }}>
-            <div><img src={ restaurant } style={{maxHeight: "150px"}}/></div>
+        <footer
+            id="footer"
+            style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 4fr",
+                padding: "2vh 10vw",
+                columnGap: "3vw",
+                backgroundColor: "#EDEFEE",
+            }}
+        >
+            <div style={{maxHeight: "18vh"}}>
+                <img src={ restaurant } style={{ maxHeight: "100%" }}/>
+            </div>
             <div style={{
                 display: "grid",
                 gridTemplateColumns: "1fr 1fr 1fr",
                 columnGap: "1vw",
-                lineHeight: 0.3,
+                lineHeight: 1.5,
                 fontSize: 14,
             }}>
                 <div>
-                    <p><b>Navigation</b></p>
-                    <p>Home</p>
-                    <p>About</p>
-                    <p>Menu</p>
-                    <p>Reservation</p>
-                    <p>Order Online</p>
-                    <p>Login</p>
+                    <ul>
+                        <li><b>Navigation</b></li>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Menu</li>
+                        <li>Reservation</li>
+                        <li>Order Online</li>
+                        <li>Login</li>
+                    </ul>
                 </div>
                 <div>
-                    <p><b>Contact</b></p>
-                    <p>Address</p>
-                    <p>Phone Number</p>
-                    <p>Email</p>
+                    <ul>
+                        <li><b>Contact</b></li>
+                        <li>Address</li>
+                        <li>Phone Number</li>
+                        <li>Email</li>
+                    </ul>
                 </div>
                 <div>
-                    <p><b>Social Media</b></p>
-                    <p>Instagram</p>
-                    <p>Twitter</p>
-                    <p>Facebook</p>
+                    <ul>
+                        <li><b>Social Media</b></li>
+                        <li><a href="https://www.instagram.com">Instagram</a></li>
+                        <li><a href="https://www.twitter.com">Twitter</a></li>
+                        <li><a href="https://www.facebook.com">Facebook</a></li>
+                    </ul>
                 </div>
             </div>
         </footer>
